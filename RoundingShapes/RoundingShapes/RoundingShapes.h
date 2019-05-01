b88ba1845145f955c2245c43505da14d9a768511
Mohamed Nagi <mohamednagi@Ehabs-BC-MacBook-Pro.local>
2019-05-01 14:14:14 +0200

    added pod codes

---
+++ RoundingShapes/RoundingShapes/RoundingShapes.h
@@ -0,0 +1,19 @@
+//
+//  RoundingShapes.h
+//  RoundingShapes
+//
+//  Created by Mohamed Nagi on 5/1/19.
+//  Copyright © 2019 Mohamed Nagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoundingShapes.
+FOUNDATION_EXPORT double RoundingShapesVersionNumber;
+
+//! Project version string for RoundingShapes.
+FOUNDATION_EXPORT const unsigned char RoundingShapesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoundingShapes/PublicHeader.h>
+
+
